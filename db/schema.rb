cab8763307be41b984f69a0d0a672323b7804133
Aleksandar Z <aleksandroz.mail@gmail.com>
2016-12-08 19:11:38 +0100

    Added devise and generated views

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224103648) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
 end
