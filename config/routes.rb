5440e99daf56a27eb0f915177898140d53b91aab
Aleksandar Z <aleksandroz.mail@gmail.com>
2016-12-09 10:27:25 +0100

    Updated routes to redirect to dashboard wor the logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'pages#new'
+
+  authenticated :user do
+    root to: "pages#dashboard", as: :user_root
+  end
+
+  unauthenticated do
+    root to: 'pages#new'
+  end
+
 end
